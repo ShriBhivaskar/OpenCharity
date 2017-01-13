18072d02f8e2b898f6ea2f10ecde76cb012baccc
ShriBhivaskar <wce.bshrinivas@gmail.com>
2017-01-14 04:49:51 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans">
+	<title>Open Charity</title>
+    <link rel="icon" href="OC.ico" >
+	<link rel="stylesheet" type="text/css" href="https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="nav" id="mynav">
+	<ul class="topnav" id="myTopnav">
+<a href="#home">	<img  class="logo" src="OCLogo.png" /></a>
+	<li class="icon" ><a href="javascript:void(0);" onclick="myFunction()">&#9776;</a></li>
+	<li><a href="#join" class="button">JOIN US</a></li>
+	<li><a href="#blog">THE BLOG</a></li>
+	<li><a href="#about">ABOUT OPEN CHARITY</a></li>
+	</ul>
+</div>
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+	
+}
+</script>
+
+
+<div class="headerr">
+<div class="head">
+<h1>SHARING IDEAS FOR CHARITIES</h1>
+    	<p class="p1" style="font-size: 16px;">Many charities' goals are similar, as is the functionality we require, but little<br> shared working takes place.</p><BR>
+		<p class="p9" >By working together, driving shared areas of interest and influencing open source<br> 
+		developments we can bring efficiencies, improve the digital experience for our users, and<br>
+		have great impact.</p><BR>
+		<p class="p1" style="font-size: 16px;">Together we can make a bigger diference.</p>
+
+</div>
+</div>
+		
+<div class="gridcontainer">
+	<div class="gridwrapper">
+		<div class="gridl">
+			<p class="p2"><span class="season">Next Event: </span> <span class="date">June 23rd 2016  18:30 - 21:00</span></br>
+			Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD</p>
+		</div>
+	<div class="gridr">
+			<a href="#register" class="button1">REGISTER</a>
+	</div>
+	</div>
+</div>
+
+<div class="gridcontainer1">
+    <div class="gridwrapper1">
+            <div>
+                <h2>GET INVOLVED</h2>
+            </div>
+		<div class="getout">
+        <div class="first-div">
+		    <a href="#meetup"><img class="centered" src="meetup.jpg" />
+            <p class="p8">WE DO MEETINGS</p></a><br>
+			<p class="p3">We organise our meetings through the OpenCharity MeetUp group</p><br>
+            <p class="p3"><a href="#meetup" class="button">MEETUP GROUP</a></p>
+        </div>
+		
+	    <div class="second-div">
+            <a href="#slack"><img class="centered" src="slack.png" />
+            <p class="p8">WE COLLABORATE</p></a><br>
+			<p class="p3">OpenCharity have a slack group for daily collaboration</p><br>
+			<p class="p3"><a href="#meetup" class="button">SLACK GROUP</a></p>
+			
+        </div>
+		
+	    <div class="third-div">
+		    <a href="#google"><img class="centered" src="google.png" />
+            <p class="p8">WE SHARE</p></a><br>
+			<p class="p3">We have a Google Group set up to share tools and documents</p><br>
+            <p class="p3"><a href="#meetup" class="button">GOOGLE GROUP</a></p>
+        </div>
+		</div>
+	</div>
+</div>
+		
+		<div class="gridcontainer">
+	            <div class="headermission">
+                <h2>OUR MISSION</h2>
+				<p class="p3">Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.</p>
+				<p class="p7">If you are a charity or a supplier, we are ready to welcome you.</p>
+            </div>	
+		</div>
+		
+<div class="gridcontainer">
+    <div class="gridwrappermission">
+        
+        <div class="gridmenu">
+            <div class="menuitembulb">
+            <p class="missp"><span class="miss">We help charities</span></p><p class="p3">
+share knowledge and working practice to make the best technology choices.</p></div>
+    
+        </div>
+		<div class="gridmenu">
+            <div class="menuitemppl">
+            <p class="missp"><span class="miss">We bring together</span></p><p class="p3">
+charities and suppliers to the charity sector to share best practices.</p></div>
+    
+        </div>
+	    <div class="gridmenu">
+            <div class="menuitemlike">
+            <p class="missp"><span class="miss">We encourage</span></p><p class="p3">
+collaboration and innovation for the good of the charity sector.</p></div>
+		
+		</div>		
+	</div>
+</div>
+
+<div class="gridcontainer">	
+		<hr class="hr1">
+		<div class="members">
+		<h2>OUR MEMBERS</h2>
+		</div>
+</div>
+
+<div class="gridcontainer">		
+<div class="gridcontainermembers">
+
+	    <div class="cancer">
+            <img src="Vector.png" width="140px" />
+		</div>
+	    <div class="ccs">
+            <img src="ccs.png" width="140px" />
+		</div>
+	    <div class="kop">
+            <img src="kop.png" width="100px" />
+        </div>		
+	    <div class="zing">
+            <img src="zing.png" width="80px" />
+        </div>	
+	    <div class="comics">
+            <img src="comic.png" width="90px"  />
+        </div>			
+		
+	</div>	
+</div>				
+
+		    <div class="headerblog">
+                <h2>BLOG</h2>
+			</div>
+<div class="gridcontainerblog">
+    <div class="gridcontainermembersblog">
+		
+		<a class="w3-btn-floating w3-hover-dark-grey w3-display-left" onclick="plusDivs(-1)">&#10094;</a>
+		<a class="w3-btn-floating w3-hover-dark-grey w3-display-right" onclick="plusDivs(1)">&#10095;</a>
+		
+        <div class="gridblogmenu">
+            <span class="season"><p>Online Donations Special...</p></span>
+			<hr class="blog">
+<p class="p5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<hr class="blog">
+            <span class="date">14 Nov 2014</span>
+		</div>
+		
+        <div class="gridblogmenu">
+            <span class="season"><p>Online Donations Special...</p></span>
+			<hr class="blog">
+<p class="p5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<hr class="blog">
+            <span class="date">14 Nov 2014</span>
+		</div>
+	    
+        <div class="gridblogmenu">
+            <span class="season"><p>Online Donations Special...</p></span>
+			<hr class="blog">
+<p class="p5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<hr class="blog">
+            <span class="date">14 Nov 2014</span>
+		</div>
+		
+        <div class="gridblogmenu">
+            <span class="season"><p>Online Donations Special...</p></span>
+			<hr class="blog">
+<p class="p5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<hr class="blog">
+            <span class="date">14 Nov 2014</span>
+		</div>		
+	</div>
+</div>		
+
+<div class="footer" style="overflow:hidden">
+<a href="#"><i style="font-size:18px" class="fa">&#xf09a;</i></a>&nbsp&nbsp&nbsp&nbsp
+<a href="#"><i style="font-size:18px" class="fa">&#xf099;</i></a>&nbsp&nbsp&nbsp&nbsp
+<a href="#"><i style="font-size:18px" class="fa">&#xf0d5;</i></a>&nbsp&nbsp&nbsp&nbsp
+<br>
+<hr class="style-six">
+
+<p>This site was built as a collaboration between <a href="https://manifesto.co.uk" >Manifesto Digital</a> and <a href="https://www.compucorp.co.uk/" >Compucorp</a></p>
+
+</div>
+
+
+
+</body>
+</html>
