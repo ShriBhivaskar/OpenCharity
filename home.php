7b6a4a07fe15dcc30e6ad55e08de39b9432aa3d4
ShriBhivaskar <wce.bshrinivas@gmail.com>
2017-01-14 04:56:56 +0530

    Update home.php

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
     <link rel="icon" href="OC.ico" >
 	<link rel="stylesheet" type="text/css" href="https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+	<link rel="stylesheet" type="text/css" href="layout.css" />
 </head>
 <body>
 <div class="nav" id="mynav">
